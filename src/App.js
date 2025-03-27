a52aac6152b0fa6514f16fb359757c748f86c268
mohit <reactnative8055@gmail.com>
2025-03-27 17:13:36 +0530

    refactor the code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import React, { useState } from "react";
 import { calculateSum } from "./Utils";
+
 export default function App() {
+  {
+    //---------- state
+  }
   const [input, setInput] = useState("");
   const [result, setResult] = useState(0);
   const [error, setError] = useState("");
 
+  //---------- helper function
   const handleCalculate = () => {
     if (!input.trim()) {
       setError("Please enter numbers");
@@ -28,24 +33,45 @@ export default function App() {
     setError("");
   };
 
+  {
+    //---------- main view
+  }
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-6 rounded-lg shadow-lg w-96">
+        {
+          //--------- ttop title
+        }
         <h1 className="text-2xl font-bold mb-4">String Calculator</h1>
+        {
+          //---------- inut field
+        }
         <input
           className="w-full p-2 border rounded mb-4"
           placeholder="Enter numbers"
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
+        {
+          //---------- submit btn
+        }
         <button
           className="bg-blue-500 text-white px-4 py-2 rounded w-full hover:bg-blue-600"
           onClick={handleCalculate}
         >
           Calculate
         </button>
+        {
+          //---------- error
+        }
         {error && <div className="mt-4 text-red-600 font-bold">{error}</div>}
+        {
+          //---------- result section
+        }
         <div className="mt-4 text-green-600 font-bold">Result: {result}</div>
+        {
+          //---------- reset btn
+        }
         <button
           className="mt-4 bg-gray-500 text-white px-4 py-2 rounded w-full hover:bg-gray-600"
           onClick={handleReset}
--- src/Utils.js
+++ src/Utils.js
@@ -1,13 +1,18 @@
 export function calculateSum(strings) {
+  // trim the string
   if (!strings.trim()) {
     return 0;
   }
+  // get number from string
   const sanitizedString = strings.replace(/,+$/, "");
   const numbers = sanitizedString.match(/-?\d+/g);
 
+  // return error if no numbers are there
   if (!numbers || sanitizedString.split(",").some((num) => num.trim() === "")) {
     throw new Error("Invalid input: Only numbers and commas are allowed");
   }
+
+  // get numeric array from elements and seprate the negative and positive numbers
   const numArray = numbers.map((num) => parseInt(num, 10));
   const negativeNumbers = numArray.filter((num) => num < 0);
   if (negativeNumbers.length > 0) {
